f0df23a434fa64ec746d7481d6aef52bceb95968
Meetanshi Gupta <gupta.mishi2508@gmail.com>
2023-10-01 01:11:39 +0530

    database created and connected

--- Flask-3.py
+++ Flask-3.py
@@ -1,8 +1,12 @@
 # about static and template folder
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def home():
